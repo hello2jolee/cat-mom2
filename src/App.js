f2bfeb52715dac275dd9bf716baec99f553d0d4c
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-07-30 22:31:33 +0900

    #2 로그인 페이지CSS 작업

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classNames from "classnames/bind";
 import AppRouter from "AppRouter";
+import { authService } from "FBase";
 
 import styles from "./App.module.scss";
 
@@ -10,11 +11,40 @@ import Loading from "assets/IconSvg/Loading";
 const cx = classNames.bind(styles);
 
 function App() {
-  const init = true;
+  const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+      } else {
+        setUserObj(null);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
   return (
-    <div className={cx("article", !init && 'type-full')}>
+    <div className={cx("article", !init && "type-full")}>
       {init ? (
-        <AppRouter />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         <div className={cx("loading-area")}>
           <Loading width={350} height={350} />
--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames/bind";
+
+import styles from "./AuthForm.module.scss";
+
+const cx = classNames.bind(styles);
 
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  const [newAccount, setNewAccount] = useState(true);
+
+  return (
+    <div className={cx("article")}>
+      <form>
+        <div className={cx("input-area")}>
+          <input type="email" placeholder="이메일 주소를 입력해주세요." />
+        </div>
+        <div className={cx("input-area")}>
+          <input
+            type="password"
+            placeholder="비밀번호는 6자 이상, 특수문자와 숫자를 포함시켜주세요."
+          />
+        </div>
+        <div className={cx("button-area")}>
+          <input
+            type="submit"
+            value={newAccount ? "가입하기" : "로그인하기"}
+            className={cx("button-submit")}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AuthForm;
